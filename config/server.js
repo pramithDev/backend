b34559998d743bc8d55c0c447f2c499a54b14898
Pramith Chathuranga <pramith@intervest.lk>
2021-06-02 23:19:09 +0530

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 5000),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '49475a96a506996e3a0ca8a6e85f34a9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0eb024a7-7c22-4ead-b4c6-27cef17506a3'
+};
