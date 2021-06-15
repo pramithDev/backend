c84fb8c67cdd68f795310f87a283f9a5b0d44ff9
Pramith Chathuranga <pramith@intervest.lk>
2021-06-16 01:31:33 +0530

    new changes...

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT'),
+  port: env.int('PORT', 5000),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '49475a96a506996e3a0ca8a6e85f34a9'),
